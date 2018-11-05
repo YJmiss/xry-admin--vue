f576a4ddb2a39e86d9053016f91071dd83aa2607
YJmiss <yi_chenyi@aliyun.com>
2018-11-05 11:37:03 +0800

    测试

--- src/views/layout/main-navbar.vue
+++ src/views/layout/main-navbar.vue
@@ -2,8 +2,8 @@
   <nav class="site-navbar" :class="'site-navbar--' + navbarLayoutType">
     <div class="site-navbar__header">
       <h1 class="site-navbar__brand" @click="$router.push({ name: 'home' })">
-        <a class="site-navbar__brand-lg" href="javascript:;">OS-ADMIN</a>
-        <a class="site-navbar__brand-mini" href="javascript:;">OS</a>
+        <a class="site-navbar__brand-lg" href="javascript:;">学而用后台系统管理</a>
+        <a class="site-navbar__brand-mini" href="javascript:;">学而用</a>
       </h1>
     </div>
     <div class="site-navbar__body clearfix">
--- static/config/index.js
+++ static/config/index.js
@@ -1,7 +1,7 @@
 /**
  * 开发环境
  */
-;(function () {
+(function() {
   window.SITE_CONFIG = {}
   // api接口请求地址
   window.SITE_CONFIG['baseUrl'] = 'http://localhost:8080/os-admin'
