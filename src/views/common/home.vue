e8e4e89f429dcb14328e1dca9ca9e357654b906d
YJmiss <yi_chenyi@aliyun.com>
2018-11-07 16:54:59 +0800

    测试

--- src/views/common/home.vue
+++ src/views/common/home.vue
@@ -2,7 +2,7 @@
   <div class="mod-home">
     <h3>项目介绍</h3>
     <ul>
-      <li>基于 Spring Boot 2.0 + Shiro + Mybatis + Vue2.0 + Element 实现的权限系统</li>
+      <li>基于 Spring Boot 2.0 + Shiro + Mybatis + Vue2.0 + Element 实现的学而用后台系统</li>
     </ul>
   </div>
 </template>
