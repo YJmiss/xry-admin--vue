31a85de19b3208897dc666bc0df2c16b212cd2c6
wujunquan <821282316@qq.com>
2018-11-09 10:26:50 +0800

    修改

--- src/views/modules/sys/course-cat.vue
+++ src/views/modules/sys/course-cat.vue
@@ -14,8 +14,8 @@
       <el-table-column prop="status" header-align="center" align="center" width="80" label="状态"></el-table-column>
       <el-table-column fixed="right" header-align="center" align="center" width="150" label="操作">
         <template slot-scope="scope">
-          <el-button v-if="isAuth('xry:course:cat:update')" type="text" size="small" @click="addOrUpdateHandle(scope.row.menuId)">修改</el-button>
-          <el-button v-if="isAuth('xry:course:cat:delete')" type="text" size="small" @click="deleteHandle(scope.row.menuId)">删除</el-button>
+          <el-button v-if="isAuth('xry:course:cat:update')" type="text" size="small" @click="addOrUpdateHandle(scope.row.id)">修改</el-button>
+          <el-button v-if="isAuth('xry:course:cat:delete')" type="text" size="small" @click="deleteHandle(scope.row.id)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
