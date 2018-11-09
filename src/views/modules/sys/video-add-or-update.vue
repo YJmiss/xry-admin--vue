1e9a9aa68eed1e0eb2d11d5c1455fde74422a057
wujunquan <821282316@qq.com>
2018-11-09 15:09:36 +0800

    添加

---
+++ src/views/modules/sys/video-add-or-update.vue
@@ -0,0 +1,143 @@
+<template>
+  <el-dialog
+    :title="!dataForm.id ? '新增' : '修改'"
+    :close-on-click-modal="false"
+    :visible.sync="visible">
+    <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()" label-width="80px">
+      <el-form-item label="视频标题" prop="title">
+        <el-input v-model="dataForm.title" type="text"placeholder="视频标题"></el-input>
+      </el-form-item>
+      <el-form-item label="视频路径" prop="videoUrl">
+        <el-input v-model="dataForm.videoUrl" type="text" placeholder="视频路径"></el-input>
+      </el-form-item>
+      <el-form-item label="所属课程" prop="courseId"> 
+        <el-input v-model="dataForm.courseId" type="text" placeholder="所属课程"></el-input>
+      </el-form-item>
+      <el-form-item label="所属目录" prop="catalogId">
+        <el-input v-model="dataForm.catalogId" placeholder="所属目录"></el-input>
+      </el-form-item>
+      <el-form-item label="是否收费" size="mini" prop="property">
+        <el-radio-group v-model="dataForm.property">
+          <el-radio :label="1">试学</el-radio>
+          <el-radio :label="2">收费</el-radio>
+          <el-radio :label="3">免费</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="审核状态" size="mini" prop="status">
+        <el-radio-group v-model="dataForm.status">
+          <el-radio :label="1">未审核</el-radio>
+          <el-radio :label="2">审核中</el-radio>
+          <el-radio :label="3">已审核</el-radio>
+          <el-radio :label="4">未通过</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="参数数据" prop="paramData">
+        <el-input v-model="dataForm.paramData" type="text"placeholder="参数数据"></el-input>
+      </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取消</el-button>
+      <el-button type="primary" @click="dataFormSubmit()">确定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+  import { isEmail, isMobile } from '@/utils/validate'
+  export default {
+    data () {
+      return {
+        visible: false,
+        roleList: [],
+        dataForm: {
+          id: 0,
+          title: '',
+          videoUrl: '',
+          courseId: '',
+          catalogId: '',
+          property: 1,
+          status: 1,
+          paramData: ''
+        },
+        dataRule: {
+          title: [
+            { required: true, message: '请填写视频标题', trigger: 'blur' }
+          ],
+          courseId: [
+            { required: true, message: '请选择视频所属课程', trigger: 'blur' }
+          ],
+          catalogId: [
+            { required: true, message: '请选择视频所属目录', trigger: 'blur' }
+          ],
+          property: [
+            { required: true, message: '请设置课程属性', trigger: 'blur' }
+          ],
+          status: [
+            { required: true, message: '请设置课程审核状态', trigger: 'blur' }
+          ],
+          paramData: [
+            { required: true, message: '请填写视频格式', trigger: 'blur' }
+          ]
+        }
+      }
+    },
+    methods: {
+      init (id) {
+        this.dataForm.id = id || 0
+        if (this.dataForm.id) {
+            this.$http({
+              url: this.$http.adornUrl(`/xry/video/info/${this.dataForm.id}`),
+              method: 'get',
+              params: this.$http.adornParams()
+            }).then(({ data }) => {
+              console.log(data);
+              if (data && data.code === 0) {
+                this.dataForm.title = data.video.title
+                this.dataForm.videoUrl = data.video.videoUrl
+                this.dataForm.courseId = data.video.courseId
+                this.dataForm.catalogId = data.video.catalogId
+                this.dataForm.property = data.video.property
+                this.dataForm.status = data.video.status
+                this.dataForm.paramData = data.video.paramData
+              }
+            })
+          }
+      },
+      // 表单提交
+      dataFormSubmit () {
+        this.$refs['dataForm'].validate((valid) => {
+          if (valid) {
+            this.$http({
+              url: this.$http.adornUrl(`/xry/video/${!this.dataForm.id ? 'save' : 'update'}`),
+              method: 'post',
+              data: this.$http.adornData({
+                'id': this.dataForm.id || undefined,
+                'title': this.dataForm.title,
+                'videoUrl': this.dataForm.videoUrl,
+                'courseId': this.dataForm.courseId,
+                'catalogId': this.dataForm.catalogId,
+                'property': this.dataForm.property,
+                'status': this.dataForm.status,
+                'paramData': this.dataForm.paramData
+              })
+            }).then(({ data }) => {
+              if (data && data.code === 0) {
+                this.$message({
+                  message: '操作成功',
+                  type: 'success',
+                  duration: 1500,
+                  onClose: () => {
+                    this.visible = false
+                    this.$emit('refreshDataList')
+                  }
+                })
+              } else {
+                this.$message.error(data.msg)
+              }
+            })
+          }
+        })
+      }
+    }
+  }
+</script>
---
+++ src/views/modules/sys/video.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="mod-video">
+    <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
+      <el-form-item>
+        <el-input v-model="dataForm.title" placeholder="视频标题" clearable></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="getDataList()">查询</el-button>
+        <el-button v-if="isAuth('xry:video:save')" type="primary" @click="addOrUpdateHandle()">新增</el-button>
+        <el-button v-if="isAuth('xry:video:delete')" type="danger" @click="deleteHandle()" :disabled="dataListSelections.length <= 0">批量删除</el-button>
+      </el-form-item>
+    </el-form>
+    <el-table :data="dataList" border v-loading="dataListLoading" @selection-change="selectionChangeHandle" style="width: 100%;">
+      <el-table-column type="selection" header-align="center" align="center" width="50">
+      </el-table-column>
+      <el-table-column prop="id" header-align="center" align="center" width="80" label="ID"></el-table-column>
+      <el-table-column prop="title" header-align="center" align="center" label="视频标题"></el-table-column>
+      <el-table-column prop="courseId" header-align="center" align="center" label="所属课程"></el-table-column>
+      <el-table-column prop="catalogId" header-align="center" align="center" label="所属目录"></el-table-column>
+      <el-table-column prop="property" header-align="center" align="center" label="是否收费">
+        <template slot-scope="scope">
+          <el-tag v-if="scope.row.property === 1" size="small" type="warning">试学</el-tag>
+          <el-tag v-else-if="scope.row.property === 2" size="small" type="danger">收费</el-tag>
+          <el-tag v-else-if="scope.row.property === 3" size="small" type="success">免费</el-tag>
+          <el-tag v-else size="small" type="warning">试学</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column prop="status" header-align="center" align="center" label="审核状态">
+        <template slot-scope="scope">
+          <el-tag v-if="scope.row.status === 1" size="small" type="warning">未审核</el-tag>
+          <el-tag v-else-if="scope.row.status === 2" size="small" type="danger">审核中</el-tag>
+          <el-tag v-else-if="scope.row.status === 3" size="small" type="success">已通过</el-tag>
+          <el-tag v-else-if="scope.row.status === 4" size="small" type="warning">未通过</el-tag>
+          <el-tag v-else size="small" type="warning">未审核</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column prop="paramData" header-align="center" align="center" label="参数数据"></el-table-column>
+      <el-table-column prop="created" header-align="center" align="center" width="180" label="创建时间"></el-table-column>
+      <el-table-column fixed="right" header-align="center" align="center" width="150" label="操作">
+        <template slot-scope="scope">
+          <el-button v-if="isAuth('xry:video:update')" type="text" size="small" @click="addOrUpdateHandle(scope.row.id)">修改</el-button>
+          <el-button v-if="isAuth('xry:video:delete')" type="text" size="small" @click="deleteHandle(scope.row.id)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination @size-change="sizeChangeHandle" @current-change="currentChangeHandle" :current-page="pageIndex" :page-sizes="[10, 20, 50, 100]" 
+          :page-size="pageSize" :total="totalPage" layout="total, sizes, prev, pager, next, jumper">
+    </el-pagination>
+    <!-- 弹窗, 新增 / 修改 -->
+    <add-or-update v-if="addOrUpdateVisible" ref="addOrUpdate" @refreshDataList="getDataList"></add-or-update>
+  </div>
+</template>
+
+<script>
+  import AddOrUpdate from './video-add-or-update'
+  export default {
+    data () {
+      return {
+        dataForm: {
+          title: ''
+        },
+        dataList: [],
+        pageIndex: 1,
+        pageSize: 10,
+        totalPage: 0,
+        dataListLoading: false,
+        dataListSelections: [],
+        addOrUpdateVisible: false
+      }
+    },
+    components: {
+      AddOrUpdate
+    },
+    activated () {
+      this.getDataList()
+    },
+    methods: {
+      // 获取数据列表
+      getDataList () {
+        this.dataListLoading = true
+        this.$http({
+          url: this.$http.adornUrl('/xry/video/list'),
+          method: 'get',
+          params: this.$http.adornParams({
+            'page': this.pageIndex,
+            'limit': this.pageSize,
+            'title': this.dataForm.title
+          })
+        }).then(({ data }) => {
+          if (data && data.code === 0) {
+            this.dataList = data.page.list
+            this.totalPage = data.page.totalCount
+          } else {
+            this.dataList = []
+            this.totalPage = 0
+          }
+          this.dataListLoading = false
+        })
+      },
+      // 每页数
+      sizeChangeHandle (val) {
+        this.pageSize = val
+        this.pageIndex = 1
+        this.getDataList()
+      },
+      // 当前页
+      currentChangeHandle (val) {
+        this.pageIndex = val
+        this.getDataList()
+      },
+      // 多选
+      selectionChangeHandle (val) {
+        this.dataListSelections = val
+      },
+      // 新增 / 修改
+      addOrUpdateHandle (id) {
+        this.addOrUpdateVisible = true
+        this.$nextTick(() => {
+          this.$refs.addOrUpdate.init(id)
+        })
+      },
+      // 删除
+      deleteHandle (id) {
+        var ids = id ? [id] : this.dataListSelections.map(item => {
+          return item.id
+        })
+        this.$confirm(`确定对[id=${ids.join(',')}]进行[${id ? '删除' : '批量删除'}]操作?`, '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$http({
+            url: this.$http.adornUrl('/xry/video/delete'),
+            method: 'post',
+            data: this.$http.adornData(ids, false)
+          }).then(({ data }) => {
+            if (data && data.code === 0) {
+              this.$message({
+                message: '操作成功',
+                type: 'success',
+                duration: 1500,
+                onClose: () => {
+                  this.getDataList()
+                }
+              })
+            } else {
+              this.$message.error(data.msg)
+            }
+          })
+        }).catch(() => {})
+      }
+    }
+  }
+</script>
