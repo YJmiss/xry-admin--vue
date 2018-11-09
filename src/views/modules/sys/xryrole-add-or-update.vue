49f1250aa86b68eefdccad32275433c5bccb13c7
wujunquan <821282316@qq.com>
2018-11-09 18:11:40 +0800

    添加

---
+++ src/views/modules/sys/xryrole-add-or-update.vue
@@ -0,0 +1,88 @@
+<template>
+  <el-dialog :title="!dataForm.id ? '新增' : '修改'" :close-on-click-modal="false" :visible.sync="visible">
+    <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()" label-width="80px">
+      <el-form-item label="角色名字" prop="name">
+        <el-input v-model="dataForm.name" type="text"placeholder="角色名字"></el-input>
+      </el-form-item>
+      <el-form-item label="是否可用" size="mini" prop="available">
+        <el-radio-group v-model="dataForm.available">
+          <el-radio :label="0">不可用</el-radio>
+          <el-radio :label="1">可用</el-radio>
+        </el-radio-group>
+      </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取消</el-button>
+      <el-button type="primary" @click="dataFormSubmit()">确定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+  import { isEmail, isMobile } from '@/utils/validate'
+  export default {
+    data () {
+      return {
+        visible: false,
+        roleList: [],
+        dataForm: {
+          id: 0,
+          name: '',
+          available: 0
+        },
+        dataRule: {
+          name: [
+            { required: true, message: '角色名字不能为空', trigger: 'blur' }
+          ]
+        }
+      }
+    },
+    methods: {
+      init (id) {
+        this.dataForm.id = id || 0
+        if (this.dataForm.id) {
+            this.$http({
+              url: this.$http.adornUrl(`/xry/role/info/${this.dataForm.id}`),
+              method: 'get',
+              params: this.$http.adornParams()
+            }).then(({ data }) => {
+              if (data && data.code === 0) {
+                this.dataForm.name = data.role.name
+                this.dataForm.available = data.role.available
+              }
+            })
+          }
+      },
+      // 表单提交
+      dataFormSubmit () {
+        this.$refs['dataForm'].validate((valid) => {
+          if (valid) {
+            this.$http({
+              url: this.$http.adornUrl(`/xry/role/${!this.dataForm.id ? 'save' : 'update'}`),
+              method: 'post',
+              data: this.$http.adornData({
+                'id': this.dataForm.id || undefined,
+                'name': this.dataForm.name,
+                'available': this.dataForm.available
+              })
+            }).then(({ data }) => {
+              if (data && data.code === 0) {
+                this.$message({
+                  message: '操作成功',
+                  type: 'success',
+                  duration: 1500,
+                  onClose: () => {
+                    this.visible = false
+                    this.$emit('refreshDataList')
+                  }
+                })
+              } else {
+                this.$message.error(data.msg)
+              }
+            })
+          }
+        })
+      }
+    }
+  }
+</script>
