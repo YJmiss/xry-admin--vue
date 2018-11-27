45687f0173d775303754fd7ce27d11c7d3eb2995
wsm12 <18487194104@189.cn>
2018-11-27 10:29:42 +0800

    完善视频上传

--- src/views/modules/sys/video-add-or-update.vue
+++ src/views/modules/sys/video-add-or-update.vue
@@ -33,13 +33,7 @@
         <el-input v-model="dataForm.videoUrl" type="text" placeholder="视频路径" readonly="readonly"></el-input>
       </el-form-item>
       <el-form-item label="上传视频" >
-    <el-upload class="load" drag :action="url" :before-upload="beforeUploadHandle" :on-success="successHandle" multiple :file-list="fileList">
-     <div class="iconCover">
-      <i class="el-icon-caret-right"></i>
-     </div>
-          <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-          <div class="el-upload__tip" slot="tip">只支持mp4格式的视频！</div>
-   </el-upload>
+        <UE :defaultMsg=defaultMsg :config=config ref="ue"></UE>
     </el-form-item>
      </el-form>
     <span slot="footer" class="dialog-footer">
@@ -49,9 +43,13 @@
   </el-dialog>
 </template>
 <script>
+  import UE from '@/components/ue/ue.vue'
   import { treeDataTranslate } from '@/utils'
+  const COMPONENT_NAME = 'uploader-file'
   export default {
+    components: {UE},
     data () {
+
       return {
         visible: false,
         roleList: [],
@@ -71,6 +69,7 @@
           courseName: '',
           catalogName: ''
         },
+        
         dataRule: {
           title: [
           { required: true, message: '请填写视频标题', trigger: 'blur' }
@@ -100,6 +99,11 @@
         courseCatalogListTreeProps: {
           label: 'title',
           children: 'children'
+        },
+         defaultMsg: '',
+        config: {
+          initialFrameWidth: null,
+          initialFrameHeight: 350
         }
       }
     },
@@ -120,47 +124,51 @@
             this.$refs['dataForm'].resetFields()
           })
         }).then(() => {
-          if (!this.dataForm.id) {
-            // 新增
-            this.courseListTreeSetCurrentNode()
-            this.courseCatalogListTreeSetCurrentNode()
-          } else {
-            this.$http({
-              url: this.$http.adornUrl(`/xry/video/info/${this.dataForm.id}`),
-              method: 'get',
-              params: this.$http.adornParams()
-            }).then(({ data }) => {
-              this.visible = true
-              if (data && data.code === 0) {
-                this.dataForm.id = data.video.id
-                this.dataForm.title = data.video.title
-                this.dataForm.videoUrl = data.video.videoUrl
-                this.dataForm.courseId = data.video.courseId
-                this.dataForm.catalogId = data.video.catalogId
-                this.dataForm.property = data.video.property
-                this.dataForm.status = data.video.status
-                this.dataForm.paramData = data.video.paramData
-                this.courseListTreeSetCurrentNode()
-                this.courseCatalogListTreeSetCurrentNode()
-              }
+          // 查询目录树，需要根据选中课程的id查询出目录树
+          this.$http({
+            url: this.$http.adornUrl('/xry/course/catalog/treeCourseCatalog'),
+            method: 'get',
+            params: this.$http.adornParams()
+          }).then(({ data }) => {
+            this.courseCatalogList = treeDataTranslate(data.courseCatalogList, 'id')
+          }).then(() => {
+            this.visible = true
+            this.$nextTick(() => {
+              this.$refs['dataForm'].resetFields()
             })
-          } 
+          }).then(() => {
+            if (!this.dataForm.id) {
+              // 新增
+              this.courseListTreeSetCurrentNode()
+              this.courseCatalogListTreeSetCurrentNode()
+            } else {
+              this.$http({
+                url: this.$http.adornUrl(`/xry/video/info/${this.dataForm.id}`),
+                method: 'get',
+                params: this.$http.adornParams()
+              }).then(({ data }) => {
+                this.visible = true
+                if (data && data.code === 0) {
+                  this.dataForm.id = data.video.id
+                  this.dataForm.title = data.video.title
+                  this.dataForm.videoUrl = data.video.videoUrl
+                  this.dataForm.courseId = data.video.courseId
+                  this.dataForm.catalogId = data.video.catalogId
+                  this.dataForm.property = data.video.property
+                  this.dataForm.status = data.video.status
+                  this.dataForm.paramData = data.video.paramData
+                  this.courseListTreeSetCurrentNode()
+                  this.courseCatalogListTreeSetCurrentNode()
+                }
+              })
+            } 
+          }) 
         })
       },
       // 课程树选中
       courseListTreeCurrentChangeHandle (data, node) {
         this.dataForm.courseId = data.id
         this.dataForm.courseName = data.title
-        // 查询目录树，需要根据选中课程的id查询出目录树
-          this.$http({
-            url: this.$http.adornUrl('/xry/course/catalog/treeCourseCatalog'),
-            method: 'get',
-            params: this.$http.adornParams({
-              'courseId':this.dataForm.courseId
-            })
-          }).then(({ data }) => {
-            this.courseCatalogList = treeDataTranslate(data.courseCatalogList, 'id')
-          }).then(() => {})
       },
       // 课程树设置当前选中节点
       courseListTreeSetCurrentNode () {
@@ -219,44 +227,30 @@
           return false
         }
       },
-     // 上传成功
-      successHandle (response, file, fileList) {
-        this.fileList = fileList
-        this.successNum++
-        if (response && response.code === 0) {
-          if (this.num === this.successNum) {
-            this.$confirm('操作成功, 是否继续操作?', '提示', {
+      //上传进度
+ progressHandle(file){
+   for(index==0;index<file.size;index++){
+  this.uplodProgress=fileList[index]/file.size
+  this.uplodProgress=this.uplodProgress*100+'%'
+   }
+   return  uplodProgress
+      },
+ // 上传成功
+successHandle(file){
+  this.progressHandle
+  if(uplodProgress=file.size){
+   this.$confirm('操作成功, 是否继续操作?', '提示', {
               confirmButtonText: '确定',
               cancelButtonText: '取消',
               type: 'warning'
             }).catch(() => {
               this.visible = false
             })
-          }
-        } else {
-          this.$message.error(response.msg)
-        }
-      }
+  }
+}
     }
   }
 </script>
 <style scoped>
-  .load{
-    text-align: center;
-    margin-top: 20px;
-    padding:10px;
-    font-size: 16px;
-    border: none;
-  }
-  .iconCover{
-    width:48px;
-    height:48px;
-    margin-left:44%;
-    margin-bottom: 20px;
-    margin-top: 40px;
-   padding: 8px;
-   border-radius: 6px;
-   border: solid 1px #bbbbbc;
-  background-color: #bbfbfb;
-  }
+
 </style>
