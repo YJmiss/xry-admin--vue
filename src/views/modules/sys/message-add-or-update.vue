f031794764816f7e722f1d2224284b775980a0ef
wujunquan <821282316@qq.com>
2018-12-04 11:17:10 +0800

    消息功能实现

--- src/views/modules/sys/message-add-or-update.vue
+++ src/views/modules/sys/message-add-or-update.vue
@@ -8,23 +8,23 @@
           <el-radio :label="3">平台通知</el-radio>
         </el-radio-group>
       </el-form-item>
-      <el-form-item label="课程消息" size="mini" prop="courseType">
+      <el-form-item label="课程消息" size="mini" prop="courseType" v-show="dataForm.type ==1">
         <el-radio-group v-model="dataForm.courseType">
           <el-radio :label="1">开课通知</el-radio>
           <el-radio :label="2">课程章节更新</el-radio>
           <el-radio :label="3">其它</el-radio>
         </el-radio-group>
       </el-form-item>
-      <el-form-item label="选择课程" prop="parentName"> 
+      <el-form-item label="选择课程" prop="parentName" v-show="dataForm.type ==1"> 
         <el-popover ref="courseListPopover" placement="bottom-start" trigger="click">
           <el-tree :data="courseList" :props="courseListTreeProps" node-key="id" ref="courseListTree"
             @current-change="courseListTreeCurrentChangeHandle" :default-expand-all="true"
             :highlight-current="true" :expand-on-click-node="false">
           </el-tree>
         </el-popover>
-        <el-input v-model="dataForm.parentName" v-popover:courseListPopover :readonly="true" placeholder="点击选择课程类目" class="cat-list__input"></el-input>
+        <el-input v-model="dataForm.parentName" v-popover:courseListPopover :readonly="true" placeholder="点击选择所属课程" class="cat-list__input"></el-input>
       </el-form-item>
-      <el-form-item label="选择讲师" prop="teacherName">
+      <el-form-item label="选择讲师" prop="teacherName" v-show="dataForm.type ==2">
         <el-popover ref="teacherListPopover" placement="bottom-start" trigger="click">
           <el-tree :data="teacherList" :props="teacherListTreeProps" node-key="id" ref="teacherListTree" @current-change="teacherListTreeCurrentChangeHandle" :default-expand-all="true"
             :highlight-current="true" :expand-on-click-node="false">
@@ -33,7 +33,7 @@
         <el-input v-model="dataForm.teacherName" v-popover:teacherListPopover :readonly="true" placeholder="点击选择所属讲师" class="cat-list__input"></el-input>
       </el-form-item>
       <el-form-item label="具体消息" prop="info">
-        <el-input v-model="dataForm.info" type="textarea"></el-input>
+        <el-input v-model="dataForm.info" type="textarea" :rows="6"></el-input>
       </el-form-item>
     </el-form>
     <span slot="footer" class="dialog-footer">
@@ -63,17 +63,10 @@
         dataListSelections: [],
         visible: false,
         dataRule: {
-          title: [{ required: true, message: '请填写课程标题', trigger: 'blur' }],
-          parentName: [{ required: true, message: '上级类目不能为空', trigger: 'change' }],
-          tid: [{ required: true, message: '请选择所属讲师', trigger: 'blur' }],
-          property: [{ required: true, message: '请设置课程属性', trigger: 'blur' }],
-          status: [{ required: true, message: '请设置课程审核状态', trigger: 'blur' }],
-          price: [ { required: true, message: '请填写课程价格', trigger: 'blur' }],
-          image: [{required: true, message: '请上传课程封面图', trigger: 'blur'}]
         },
         courseList: [],
         courseListTreeProps: {
-          label: 'name',
+          label: 'title',
           children: 'children'
         },
         teacherList: [],
@@ -115,13 +108,13 @@
                 params: this.$http.adornParams()
               }).then(({ data }) => {
                 if (data && data.code === 0) {
-                  this.dataForm.id = data.course.id
-                  this.dataForm.type = data.course.type
-                  this.dataForm.courseType = data.course.courseType
-                  this.dataForm.courseId = data.course.courseId
-                  this.dataForm.userId = data.course.userId
-                  this.dataForm.publishDate = data.course.publishDate
-                  this.dataForm.info = data.course.info
+                  this.dataForm.id = data.message.id
+                  this.dataForm.type = data.message.type
+                  this.dataForm.courseType = data.message.courseType
+                  this.dataForm.objId = data.message.objId
+                  this.dataForm.userId = data.message.userId
+                  this.dataForm.publishDate = data.message.publishDate
+                  this.dataForm.info = data.message.info
                   this.courseListTreeSetCurrentNode()
                   this.teacherListTreeSetCurrentNode()
                 }
@@ -135,57 +128,106 @@
       },
       // 课程类目树选中
       courseListTreeCurrentChangeHandle (data, node) {
-        this.dataForm.cid = data.id
-        this.dataForm.parentName = data.name
+        this.dataForm.objId = data.id
+        this.dataForm.parentName = data.title
       },
       // 课程类目树设置当前选中节点
       courseListTreeSetCurrentNode () {
-        this.$refs.courseListTree.setCurrentKey(this.dataForm.cid)
-        this.dataForm.parentName = (this.$refs.courseListTree.getCurrentNode() || {})['name']
+        this.$refs.courseListTree.setCurrentKey(this.dataForm.objId)
+        this.dataForm.parentName = (this.$refs.courseListTree.getCurrentNode() || {})['title']
       },
       // 讲师树选中
       teacherListTreeCurrentChangeHandle (data, node) {
-        this.dataForm.tid = data.id
+        this.dataForm.userId = data.id
         this.dataForm.teacherName = data.nickname
       },
       // 讲师树设置当前选中节点
       teacherListTreeSetCurrentNode () {
-        this.$refs.teacherListTree.setCurrentKey(this.dataForm.tid)
+        this.$refs.teacherListTree.setCurrentKey(this.dataForm.userId)
         this.dataForm.teacherName = (this.$refs.teacherListTree.getCurrentNode() || {})['nickname']
       },
       // 表单提交
       dataFormSubmit () {
-        this.$refs['dataForm'].validate((valid) => {
-          if (valid) {
-            this.$http({
-              url: this.$http.adornUrl(`/xry/message/${!this.dataForm.id ? 'save' : 'update'}`),
-              method: 'post',
-              data: this.$http.adornData({
-                'id': this.dataForm.id || undefined,
-                'type': this.dataForm.type,
-                'courseType': this.dataForm.courseType,
-                'courseId': this.dataForm.courseId,
-                'userId': this.dataForm.userId,
-                'publishDate': this.dataForm.publishDate,
-                'info': this.dataForm.info
-              })
-            }).then(({ data }) => {
-              if (data && data.code === 0) {
-                this.$message({
-                  message: '操作成功',
-                  type: 'success',
-                  duration: 1500,
-                  onClose: () => {
-                    this.visible = false
-                    this.$emit('refreshDataList')
-                  }
+        if (!this.dataValidate()) {
+
+        } else {
+          this.$refs['dataForm'].validate((valid) => {
+            if (valid) {
+              this.$http({
+                url: this.$http.adornUrl(`/xry/message/${!this.dataForm.id ? 'save' : 'update'}`),
+                method: 'post',
+                data: this.$http.adornData({
+                  'id': this.dataForm.id || undefined,
+                  'type': this.dataForm.type,
+                  'courseType': this.dataForm.courseType,
+                  'objId': this.dataForm.objId,
+                  'userId': this.dataForm.userId,
+                  'publishDate': this.dataForm.publishDate,
+                  'info': this.dataForm.info
                 })
-              } else {
-                this.$message.error(data.msg)
-              }
+              }).then(({ data }) => {
+                if (data && data.code === 0) {
+                  this.$message({
+                    message: '操作成功',
+                    type: 'success',
+                    duration: 1500,
+                    onClose: () => {
+                      this.visible = false
+                      this.$emit('refreshDataList')
+                    }
+                  })
+                } else {
+                  this.$message.error(data.msg)
+                }
+              })
+            }
+          })
+        }
+      },
+      // 校验form表单
+      dataValidate () {
+        let isPass = false;
+        if (this.dataForm.type == 1) {
+          // 课程消息
+          if (!this.dataForm.parentName) {
+            this.$confirm(`请选择课程`, '提示', {
+              confirmButtonText: '确定',
+              cancelButtonText: '取消',
+              type: 'warning'
+            }).then(() => {
+              isPass = false
             })
+          } else {
+            isPass = true
           }
-        })
+        } else if (this.dataForm.type == 2) {
+          // 我关注的
+          if (!this.dataForm.teacherName) {
+            this.$confirm(`请选择讲师`, '提示', {
+              confirmButtonText: '确定',
+              cancelButtonText: '取消',
+              type: 'warning'
+            }).then(() => {
+              isPass = false;
+            })
+          } else {
+            isPass = true
+          }
+        } else if (this.dataForm.type == 3) {
+          // 平台消息
+          if (!this.dataForm.info) {
+            this.$confirm(`请填写具体消息`, '提示', {
+              confirmButtonText: '确定',
+              cancelButtonText: '取消',
+              type: 'warning'
+            }).then(() => {
+              isPass = false;
+            })
+          } else {
+            isPass = true
+          }
+        }
+        return isPass;
       }
     }
   }
--- src/views/modules/sys/message.vue
+++ src/views/modules/sys/message.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="mod-course">
     <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
-      <el-form-item label="所属类目" prop="parentName"> 
-        <el-popover ref="courseCatListPopover" placement="bottom-start" trigger="click">
-          <el-tree :data="courseCatList" :props="courseCatListTreeProps" node-key="id" ref="courseCatListTree"
-            @current-change="courseCatListTreeCurrentChangeHandle" :default-expand-all="true"
+      <el-form-item label="所属课程" prop="parentName"> 
+        <el-popover ref="courseListPopover" placement="bottom-start" trigger="click">
+          <el-tree :data="courseList" :props="courseListTreeProps" node-key="id" ref="courseListTree"
+            @current-change="courseListTreeCurrentChangeHandle" :default-expand-all="true"
             :highlight-current="true" :expand-on-click-node="false">
           </el-tree>
         </el-popover>
-        <el-input v-model="dataForm.parentName" v-popover:courseCatListPopover :readonly="true" placeholder="点击选择课程类目" class="cat-list__input"></el-input>
+        <el-input v-model="dataForm.parentName" v-popover:courseListPopover :readonly="true" placeholder="点击选择所属课程" class="cat-list__input"></el-input>
       </el-form-item>
       <el-form-item label="所属讲师" prop="teacherName">
         <el-popover ref="teacherListPopover" placement="bottom-start" trigger="click">
@@ -18,8 +18,10 @@
         </el-popover>
         <el-input v-model="dataForm.teacherName" v-popover:teacherListPopover :readonly="true" placeholder="点击选择所属讲师" class="cat-list__input"></el-input>
       </el-form-item>
-      <el-form-item label="课程标题">
-        <el-input v-model="dataForm.title" placeholder="课程标题" clearable></el-input>
+      <el-form-item label="消息类型">
+        <el-select v-model="dataForm.type" placeholder="请选择消息类型" @change="currentSel">
+          <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
+        </el-select>
       </el-form-item>
       <el-form-item>
         <el-button @click="getDataList()">查询</el-button>
@@ -32,26 +34,33 @@
       <!-- <el-table-column prop="id" header-align="center" align="center" width="80" label="ID"></el-table-column> -->
       <el-table-column prop="type" header-align="center" align="center" width="150" label="消息类型">
         <template slot-scope="scope">
-          <el-tag v-if="scope.row.type === 1" size="small" type="warning">课程消息</el-tag>
+          <el-tag v-if="scope.row.type === 1" size="small" type="success">课程消息</el-tag>
           <el-tag v-else-if="scope.row.type === 2" size="small" type="danger">我关注的</el-tag>
           <el-tag v-else size="small" type="warning">平台通知</el-tag>
         </template>
       </el-table-column>
       <el-table-column prop="course_type" header-align="center" align="center" width="150" label="课程消息类型">
         <template slot-scope="scope">
-          <el-tag v-if="scope.row.course_type === 1" size="small" type="warning">开课通知</el-tag>
-          <el-tag v-else-if="scope.row.course_type === 2" size="small" type="danger">课程章节更新</el-tag>
-          <el-tag v-else size="small" type="warning">其它</el-tag>
+          <el-tag v-if="scope.row.course_type === 1" size="small" type="info">开课通知</el-tag>
+          <el-tag v-else size="small" type="success">课程章节更新</el-tag>
         </template>
       </el-table-column>
-      <el-table-column prop="course_id" header-align="center" align="left" width="320" label="消息->课程"></el-table-column>
-      <el-table-column prop="user_id" header-align="center" align="left" width="300" label="消息->讲师"></el-table-column>
-      <el-table-column prop="publish_date" header-align="center" align="center" width="150" label="发布日期"></el-table-column>
-      <el-table-column prop="info" header-align="center" align="center" label="具体信息"></el-table-column>
-      <el-table-column fixed="right" header-align="center" align="left" width="250" label="操作">
+      <el-table-column prop="courseTitle" header-align="center" align="left" width="300" label="消息->课程"></el-table-column>
+      <el-table-column prop="nickname" header-align="center" align="center" width="130" label="消息->讲师"></el-table-column>
+      <el-table-column prop="status" header-align="center" align="center" width="150" label="发布状态">
+        <template slot-scope="scope">
+          <el-tag v-if="scope.row.status === 0" size="small" type="info">未发布</el-tag>
+          <el-tag v-else size="small" type="success">已发布</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column prop="publish_date" header-align="center" align="center" width="200" label="发布日期"></el-table-column>
+      <el-table-column prop="info" header-align="center" align="left" label="具体信息" width="350"></el-table-column>
+      <el-table-column fixed="right" header-align="center" align="left" width="230" label="操作">
         <template slot-scope="scope" porp="status">
           <el-button v-if="isAuth('xry:message:update')" type="primary" size="small" icon="el-icon-edit" circle @click="addOrUpdateHandle(scope.row.id)"></el-button>
           <el-button v-if="isAuth('xry:message:delete')" type="danger" size="small" icon="el-icon-delete" circle @click="deleteHandle(scope.row.id)"></el-button>
+          <el-button v-if="isAuth('xry:message:publishMessage')" type="primary" round size="small" @click="publishMessage(scope.row.id)" v-show="scope.row.status ===0">立即发布</el-button>
+          <el-button v-if="isAuth('xry:message:cancelPublish')" type="warning" round size="small" @click="cancelPublish(scope.row.id)" v-show="scope.row.status ===1 ">取消发布</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -72,10 +81,13 @@
         dataForm: {
           type: '',
           course_type: '',
-          course_id: '',
-          user_id:'',
+          status: 0,
           publish_date:'',
-          info:''
+          info:'',
+          courseTitle:'',
+          nickname:'',
+          parentName:'',
+          teacherName:''
         },
         dataList: [],
         pageIndex: 1,
@@ -84,16 +96,22 @@
         dataListLoading: false,
         dataListSelections: [],
         addOrUpdateVisible: false,
-        courseCatList: [],
-        courseCatListTreeProps: {
-          label: 'name',
+        courseList: [],
+        courseListTreeProps: {
+          label: 'title',
           children: 'children'
         },
         teacherList: [],
         teacherListTreeProps: {
           label: 'nickname',
           children: 'children'
-        }
+        },
+        options: [
+          { value: '1', label: '课程消息' }, 
+          { value: '2', label: '我关注的' },
+          { value: '3', label: '平台通知' }
+        ],
+        value: ''
       }
     },
     components: {
@@ -108,11 +126,11 @@
         this.dataListLoading = true
         // 查询所有课程类目，构造成一棵树
         this.$http({
-          url: this.$http.adornUrl('/xry/course/cat/treeCourseCat'),
+          url: this.$http.adornUrl('/xry/course/treeCourse'),
           method: 'get',
           params: this.$http.adornParams()
         }).then(({ data }) => {
-          this.courseCatList = treeDataTranslate(data.courseCatList, 'id')
+          this.courseList = treeDataTranslate(data.courseList, 'id')
         }).then(() => {
           // 查询讲师列表，构造成一棵树
           this.$http({
@@ -123,11 +141,14 @@
             this.teacherList = treeDataTranslate(data.userList, 'id')
           }).then(() => {
             this.$http({
-              url: this.$http.adornUrl('/xry/course/list'),
+              url: this.$http.adornUrl('/xry/message/list'),
               method: 'get',
               params: this.$http.adornParams({
                 'page': this.pageIndex,
-                'limit': this.pageSize
+                'limit': this.pageSize,
+                'objId': this.dataForm.objId,
+                'userId':this.dataForm.userId,
+                'type':this.dataForm.type
               })
             }).then(({ data }) => {
               if (data && data.code === 0) {
@@ -142,24 +163,24 @@
           })
         })
       },
-      // 课程类目树选中
-      courseCatListTreeCurrentChangeHandle (data, node) {
-        this.dataForm.parentId = data.id
-        this.dataForm.parentName = data.name
+      // 课程树选中
+      courseListTreeCurrentChangeHandle (data, node) {
+        this.dataForm.objId = data.id
+        this.dataForm.parentName = data.title
       },
-      // 课程类目树设置当前选中节点
-      courseCatListTreeSetCurrentNode () {
-        this.$refs.courseCatListTree.setCurrentKey(this.dataForm.parentId)
-        this.dataForm.parentName = (this.$refs.courseCatListTree.getCurrentNode() || {})['name']
+      // 课程树设置当前选中节点
+      courseListTreeSetCurrentNode () {
+        this.$refs.courseListTree.setCurrentKey(this.dataForm.objId)
+        this.dataForm.parentName = (this.$refs.courseListTree.getCurrentNode() || {})['title']
       },
       // 讲师树选中
       teacherListTreeCurrentChangeHandle (data, node) {
-        this.dataForm.teacherId = data.id
+        this.dataForm.userId = data.id
         this.dataForm.teacherName = data.nickname
       },
       // 讲师树设置当前选中节点
       teacherListTreeSetCurrentNode () {
-        this.$refs.teacherListTree.setCurrentKey(this.dataForm.teacherId)
+        this.$refs.teacherListTree.setCurrentKey(this.dataForm.userId)
         this.dataForm.teacherName = (this.$refs.teacherListTree.getCurrentNode() || {})['nickname']
       },
       // 每页数
@@ -195,7 +216,7 @@
           type: 'warning'
         }).then(() => {
           this.$http({
-            url: this.$http.adornUrl('/xry/course/delete'),
+            url: this.$http.adornUrl('/xry/message/delete'),
             method: 'post',
             data: this.$http.adornData(ids, false)
           }).then(({ data }) => {
@@ -213,6 +234,70 @@
             }
           })
         }).catch(() => {})
+      },
+      // 发布消息
+      publishMessage (id) {
+        var ids = id ? [id] : this.dataListSelections.map(item => {
+          return item.id
+        })
+        this.$confirm(`确定对[id=${ids.join(',')}]进行[${id ? '发布' : '批量发布'}]操作?`, '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$http({
+            url: this.$http.adornUrl('/xry/message/publishMessage'),
+            method: 'post',
+            data: this.$http.adornData(ids, false)
+          }).then(({ data }) => {
+            if (data && data.code === 0) {
+              this.$message({
+                message: '操作成功',
+                type: 'success',
+                duration: 1500,
+                onClose: () => {
+                  this.getDataList()
+                }
+              })
+            } else {
+              this.$message.error(data.msg)
+            }
+          })
+        }).catch(() => {})
+      },
+      // 取消发布消息
+      cancelPublish (id) {
+        var ids = id ? [id] : this.dataListSelections.map(item => {
+          return item.id
+        })
+        this.$confirm(`确定对[id=${ids.join(',')}]进行[${id ? '取消发布' : '批量取消发布'}]操作?`, '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$http({
+            url: this.$http.adornUrl('/xry/message/cancelPublish'),
+            method: 'post',
+            data: this.$http.adornData(ids, false)
+          }).then(({ data }) => {
+            if (data && data.code === 0) {
+              this.$message({
+                message: '操作成功',
+                type: 'success',
+                duration: 1500,
+                onClose: () => {
+                  this.getDataList()
+                }
+              })
+            } else {
+              this.$message.error(data.msg)
+            }
+          })
+        }).catch(() => {})
+      },
+      // 消息类型下拉选中事件
+      currentSel(selVal){
+        this.dataForm.type = selVal;
       }
     }
   }
